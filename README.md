15f745a8c3b08329b7416fabb87ed93c367573b0
Alejandro Granadillo <slayerfat@gmail.com>
2016-07-25 15:14:04 -0400

    added basic meteor boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # todo
 todo apps on different languages / frameworks 
+
+This branch uses meteor with angular 2.
+
+To run:
+
+```bash
+meteor
+```
---
+++ client/app.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `<h1>Todo App</h1>`
+})
+export class AppComponent {
+}
