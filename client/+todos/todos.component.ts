7766f948255a2e31dccdbad4f9d9afd75ab81cc4
Alejandro Granadillo <slayerfat@gmail.com>
2016-07-26 17:36:14 -0400

    added remove todo

--- client/+todos/todos.component.ts
+++ client/+todos/todos.component.ts
@@ -1,19 +1,28 @@
 // noinspection TypeScriptCheckImport
 import template from './todos.component.html';
+import styles from './todos.component.css';
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../shared/collections/todos/Todo';
 import { TodoCollection } from '../../shared/collections/todos/TodoCollection';
 import { AddTodoComponent } from './shared/add-todo.component';
+import { HumanDiffPipe } from '../shared/pipes/human-diff.pipe';
+// noinspection TypeScriptCheckImport
 
 @Component({
   selector: 'todos',
   template,
-  directives: [AddTodoComponent]
+  styles,
+  directives: [AddTodoComponent],
+  pipes: [HumanDiffPipe]
 })
 export class TodosComponent implements OnInit {
   public todos: Mongo.Cursor<Todo>;
 
   public ngOnInit(): any {
     this.todos = TodoCollection.find();
+  }
+
+  public removeTodo(todo: Todo): void {
+    TodoCollection.remove(todo._id);
   }
 }
