ea1ed5b5605bfeaeb17cf1e3ddb9893666e6d441
Alejandro Granadillo <slayerfat@gmail.com>
2016-07-26 14:30:30 -0400

    added font-awesome and foundation

--- client/+todos/todos.component.ts
+++ client/+todos/todos.component.ts
@@ -4,7 +4,10 @@ import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../shared/collections/todos/Todo';
 import { TodoCollection } from '../../shared/collections/todos/TodoCollection';
 
-@Component({template})
+@Component({
+  selector: 'todos',
+  template
+})
 export class TodosComponent implements OnInit {
   public todos: Mongo.Cursor<Todo>;
 
---
+++ post-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# runs after every npm install
+
+# copy FontAwesome
+cp -R node_modules/font-awesome/fonts ./public/fonts
