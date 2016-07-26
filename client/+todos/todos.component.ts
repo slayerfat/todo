3d234bea253bfd8eb1744d00274f12b0938ee477
Alejandro Granadillo <slayerfat@gmail.com>
2016-07-26 19:23:31 -0400

    todos can be updated

--- client/+todos/todos.component.ts
+++ client/+todos/todos.component.ts
@@ -1,13 +1,14 @@
 // noinspection TypeScriptCheckImport
 import template from './todos.component.html';
+
+// noinspection TypeScriptCheckImport
 import styles from './todos.component.css';
+
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../shared/collections/todos/Todo';
 import { TodoCollection } from '../../shared/collections/todos/TodoCollection';
 import { AddTodoComponent } from './shared/add-todo.component';
 import { HumanDiffPipe } from '../shared/pipes/human-diff.pipe';
-// noinspection TypeScriptCheckImport
-// noinspection TypeScriptCheckImport
 
 @Component({
   selector: 'todos',
@@ -18,6 +19,9 @@ import { HumanDiffPipe } from '../shared/pipes/human-diff.pipe';
 })
 export class TodosComponent implements OnInit {
   public todos: Mongo.Cursor<Todo>;
+  private updating: Todo;
+  private todoInput: string;
+  private todoInputError: boolean;
 
   public ngOnInit(): any {
     this.todos = TodoCollection.find();
@@ -31,13 +35,32 @@ export class TodosComponent implements OnInit {
     });
   }
 
-  public updateTodo(todo: Todo): void {
-    console.log('show contents input');
-    // TodoCollection.update(todo._id, {
-    //   $set: {
-    //     contents: !todo.contents
-    //   }
-    // });
+  public updateTodo(ok = true): void {
+    // if the user cancels the update,
+    // we have to update and return early
+    if (ok === false || this.updating === null) {
+      this.updating       = null;
+      this.todoInputError = null;
+      return;
+    }
+
+    if (typeof this.todoInput !== 'undefined') {
+      const input = this.todoInput.trim();
+      if (input.length >= 5 && input.length <= 255) {
+        TodoCollection.update(this.updating._id, {
+          $set: {
+            contents: this.todoInput
+          }
+        });
+
+        this.updating       = null;
+        this.todoInputError = null;
+
+        return;
+      }
+    }
+
+    this.todoInputError = true;
   }
 
   public removeTodo(todo: Todo): void {
