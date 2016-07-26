399e0557eb75c067abb418d844762becd9ee1889
Alejandro Granadillo <slayerfat@gmail.com>
2016-07-26 17:47:17 -0400

    added completed todo logic

--- client/+todos/todos.component.ts
+++ client/+todos/todos.component.ts
@@ -7,6 +7,7 @@ import { TodoCollection } from '../../shared/collections/todos/TodoCollection';
 import { AddTodoComponent } from './shared/add-todo.component';
 import { HumanDiffPipe } from '../shared/pipes/human-diff.pipe';
 // noinspection TypeScriptCheckImport
+// noinspection TypeScriptCheckImport
 
 @Component({
   selector: 'todos',
@@ -20,6 +21,23 @@ export class TodosComponent implements OnInit {
 
   public ngOnInit(): any {
     this.todos = TodoCollection.find();
+  }
+
+  public updateTodoStatus(todo: Todo): void {
+    TodoCollection.update(todo._id, {
+      $set: {
+        completed: !todo.completed
+      }
+    });
+  }
+
+  public updateTodo(todo: Todo): void {
+    console.log('show contents input');
+    // TodoCollection.update(todo._id, {
+    //   $set: {
+    //     contents: !todo.contents
+    //   }
+    // });
   }
 
   public removeTodo(todo: Todo): void {
